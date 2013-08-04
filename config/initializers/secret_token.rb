d286a7f81bd81bd7d59b80193b9f0eb1eecca955
Dahlia <fishball7557@gmail.com>
2013-08-04 14:42:19 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompaignTw::Application.config.secret_token = 'a76cea8ade2869c037ef3bbce07cd49865f77bf44cecc8f29967ce242d27a2872824ef2a263a48440d5b40e686b01e9cbdda2986e989acf0aa55f8960fd135b3'
