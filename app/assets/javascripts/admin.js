067690bff62429acaa927420415d4e80c4d19ed5
Dahlia <fishball7557@gmail.com>
2013-08-04 15:08:26 +0800

    venus:jqueryui

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,33 @@
 //= require twitter/bootstrap
 //= require unicorn
 
+//= require jquery.ui.datepicker
+//= require jquery.ui.datepicker-zh-TW
+//= require jquery.ui.slider
+//= require jquery.timepicker
+
+//= require_self
+
+
+
+
+$(document).ready(function() {
+  $('.datepicker').datepicker({
+    dateFormat: "yy-mm-dd"
+  });
+});
+
+$(document).ready(function() {
+  $(".datetimepicker").each(function(){
+    $(this).datetimepicker({
+      dateFormat: "yy-mm-dd",
+      hourGrid: 4,
+      minuteGrid: 10,
+      stepMinute: 10
+    });
+  });
+});
+
 
 $(document).on('nested:fieldAdded', function(event){
   // this field was just inserted into your form
