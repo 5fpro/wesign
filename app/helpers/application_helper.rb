faa3903f7d52ecc7b8f5ca5038bf6cb5565616c6
Dahlia <fishball7557@gmail.com>
2013-09-10 01:16:43 +0800

    pull & request 修改

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,12 @@ module ApplicationHelper
     Category.ordered.map{ |c| [c.name, c.id] }
   end
 
-  def signin_before_petition
+  def render_new_petition_link(options = {})
+    options[:data] = { :toggle => "modal" }
     if user_signed_in?
-      link_to "發起連署", "#petition-start-up", :data => { :toggle => "modal" }
+      link_to "發起連署", "#petition-start-up", options
     else
-      link_to "登入以發起連署", "#sign-up", :data => { :toggle => "modal" }
+      link_to "發起連署", "#sign-up", options
     end
   end
 
