dfb66d0e153d11a2afa42f09f344d2e0455a9f10
Dahlia <fishball7557@gmail.com>
2013-09-09 17:04:08 +0800

    pull & request 修改

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     Category.ordered.map{ |c| [c.name, c.id] }
   end
 
+  def signin_before_petition
+    if user_signed_in?
+      link_to "發起連署", "#petition-start-up", :data => { :toggle => "modal" }
+    else
+      link_to "登入以發起連署", "#sign-up", :data => { :toggle => "modal" }
+    end
+  end
+
 end
