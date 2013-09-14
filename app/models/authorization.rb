533afe42e1b3ef3e0b9ca90e30a09e0fb88d2149
marsz <marsz330@gmail.com>
2013-09-14 17:45:28 +0800

    sync fb_id to user

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -5,4 +5,18 @@ class Authorization < ActiveRecord::Base
   attr_accessible :provider, :uid, :auth_type, :auth_id, :auth_data
   serialize :auth_data, Hash
   attr_accessible :auth_data, :auth_id, :auth_type, :provider, :uid
+
+  after_create do
+    auth.update_column :fb_id, uid if is_fb_for_user?
+  end
+
+  after_destroy do
+    auth.update_column :fb_id, nil if is_fb_for_user?
+  end
+
+  private
+
+  def is_fb_for_user?
+    provider.to_s == "facebook" && auth_type == "User"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130829113042) do
+ActiveRecord::Schema.define(:version => 20130914093838) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(:version => 20130829113042) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
+    t.string   "fb_id"
     t.string   "name"
     t.string   "phone"
     t.string   "location"
@@ -96,6 +97,7 @@ ActiveRecord::Schema.define(:version => 20130829113042) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["fb_id"], :name => "index_users_on_fb_id"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
