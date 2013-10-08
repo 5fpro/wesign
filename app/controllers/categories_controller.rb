f46d6454b6508304fccc363764e595ca8ca8fc79
Dahlia <fishball7557@gmail.com>
2013-10-08 16:50:00 +0800

    首頁套版

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < BaseController
 
   def index
     @categories = Category.ordered
+    @petitions = Petition.scoped
+    @petitions_count = Petition.count
+    @petition_signs_count = Petition.sum("signs_count")
+    render :layout => "application"
   end
 
 end
